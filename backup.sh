9a59eaa76817e5180a6063daf4e7d64e17a9a160
Andrew6869 <melkiy0a@gmail.com>
2026-03-01 14:55:08 +0300

    Add .gitignore and initial backup script

--- backup.sh
+++ backup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-echo "Backup script"
-# Добавьте дополнительные строки по необходимости
-# This is a comment
+echo "Backup script for Andrew6869"
+# Добавляем простую логику
+backup_dir="./backup_$(date +%Y%m%d)"
+mkdir -p "$backup_dir"
+echo "Backup created in $backup_dir"
